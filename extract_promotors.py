96f2b0d57b88bc67cd0bdc7f9dcc2e2a8708a938
Szymon Turek <49413872+SzymonTurek@users.noreply.github.com>
2021-11-03 16:00:27 +0100

    Add files via upload

---
+++ extract_promotors.py
@@ -0,0 +1,87 @@
+#print("Hello world")
+from Bio import SeqIO
+import pandas as pd
+import re
+
+#my_sheet = 'Sheet1'
+#file_name = '/Volumes/One_Touch/promotor_extraction/gene_upregulated.xls'
+#df = read_excel(file_name, sheet_name = my_sheet, engine='openpyxl')
+
+#gene_name = df[:1]
+#print(gene_name)
+#fasta_sequences = SeqIO.parse(open('/Volumes/One_Touch/promotor_extraction/sekwencje_promotorów_hybrydowa_MP.fasta'),'fasta')
+
+input_seqs =  pd.read_csv('/Volumes/One_Touch/promotor_extraction/gene_upregulated.tsv', sep='\t')
+df = pd.DataFrame(input_seqs)
+#for gene in df['Gene']:
+#    gene_names = gene.split(".")[2]
+
+
+gene_names = [gene.split(".")[2] for gene in df['Gene']]
+#print(gene_names)
+
+ctg_positions_in_excel = [locus.split(":")[1].split("-")[0] for locus in df['Locus']]
+ctg_positions_in_excel_int = [int(contig_position) -1 for contig_position in ctg_positions_in_excel]
+#print(ctg_positions_in_excel_int[:10])
+
+ctg_positions_in_excel_int = ctg_positions_in_excel_int[:20]
+
+
+with open("/Volumes/One_Touch/promotor_extraction/promotor_output.txt","w") as f:
+       
+        for seq_record in SeqIO.parse("/Volumes/One_Touch/promotor_extraction/sekwencje_promotorów_hybrydowa_MP.fasta", "fasta"):
+            
+            ctg_position_in_dataset = int(str(seq_record.id).split(":")[3].split("-")[1])
+            #print(ctg_position_in_dataset)
+            gene_name = seq_record.name.split(":")[0]
+            #print(gene_name)
+            if gene_name in gene_names and ctg_position_in_dataset in ctg_positions_in_excel_int:
+                print(seq_record)
+                SeqIO.write(seq_record, f, "fasta")
+          
+          
+          
+          
+'''
+          
+            #print(seq_record)
+            #f.write(str(seq_record.seq[:10]) + "\n")  #first 10 base positions
+            #print(seq_record.name)
+            #ctg_position_in_dataset = str(seq_record.id).split(":")[3].split("-")[0]
+            
+            
+            for locus in df['Locus']:
+                ctg_position_in_excel = locus.split(":")[1].split("-")[0]
+                #print(ctg_position_in_excel)
+                ctg_position_in_excel_int = int(ctg_position_in_excel) - 1
+                if(ctg_position_in_excel_int == ctg_position_in_dataset):
+                    print(seq_record)
+            
+            
+            #ctg_position = str(seq_record.id).split(":")
+            
+            #print(ctg_position_in_dataset)
+            if "G18110" in seq_record.name:
+                #print(seq_record) 
+                pass
+
+
+
+
+input_seqs =  pd.read_csv('/Volumes/One_Touch/promotor_extraction/gene_upregulated.tsv', sep='\t')
+df = pd.DataFrame(input_seqs)
+#first_coulmn = df.iloc[:,0]
+#for word in first_column:
+
+#print(first_coulmn)
+#print(input_seqs)
+#print(df['Locus'])
+
+#for gene in df['Gene']:
+#    print(gene.split(".")[2])
+
+for locus in df['Locus'][:10]:
+    print(locus.split(":")[1].split("-")[0])
+#print(df['Gene'].split(".")[2])
+
+'''
